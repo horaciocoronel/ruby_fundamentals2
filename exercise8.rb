56e5cc6d97fed2ccb0638d33447013c6f44a9d8e
horaciocoronel <arielc@gmail.com>
2017-09-19 17:34:58 -0400

    Define 3 methods to follow DRY principle. Add comments for clarification

--- exercise8.rb
+++ exercise8.rb
@@ -1,27 +1,36 @@
-def speed(distance,time)
-  time = (time * 60.0).to_i
-  x = (distance / time).round(2)
+# Define a method to get distance
+def person_distance(person)
+  puts "How far did person #{person} run (in metres)?"
+  distance = gets.to_f
 end
 
-puts "How far did person 1 run (in metres)?"
-distance1 = gets.to_f
-puts "How long (in minutes) did person 1 run take to run #{distance1} metres?"
-mins1 = gets.to_f
+# Define a method to get time
+def person_time(person, distance)
+  puts "How long (in minutes) did person #{person} run take to run #{distance} metres?"
+  time = gets.to_f
+end
+# Define method to calculate distance per second
+def speed(distance,time)
+  time = (time * 60.0).to_i
+  (distance / time).round(2)
+end
 
-puts "How far did person 2 run (in metres)?"
-distance2 = gets.to_f
-puts "How long (in minutes) did person 2 take to run #{distance2} metres?"
-mins2 = gets.to_f
+# Call the methods and put them inside variables
+person1_d = person_distance(1)
+person1_t = person_time(1, person1_d)
 
-puts "How far did person 3 run (in metres)?"
-distance3 = gets.to_f
-puts "How long (in minutes) did person 3 take to run #{distance3} metres?"
-mins3 = gets.to_f
+person2_d = person_distance(2)
+person2_t = person_time(2, person2_d)
 
-speed1 = speed(distance1, mins1)
-speed2 = speed(distance2, mins2)
-speed3 = speed(distance3, mins3)
+person3_d = person_distance(3)
+person3_t = person_time(2, person3_t)
 
+# Put speed per person into variables
+speed1 = speed(person1_d, person1_t)
+speed2 = speed(person2_d, person2_t)
+speed3 = speed(person3_d, person3_t)
+
+# Logic to determine who is the fastest runner
 if speed3 > speed2 && speed3 > speed1
   puts "Person 3 was the fastest at #{speed3} m/s"
 elsif speed2 > speed3 && speed2 > speed1
